2edda6b6c4cc268d2248e57f60cbf1dc84c98fe7
Arafat Hossain <Arafat.Hossain@brainstation-23.com>
2023-11-01 12:20:09 +0600

    added view model for blog page

---
+++ Blog/Models/BlogViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models
+{
+    public class BlogViewModel
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string ThumbnailImageUrl { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
